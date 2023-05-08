b19412495c68afa1dec70de8ef402dcbefd570db
jatinjd17 <69747287+jatinjd17@users.noreply.github.com>
2023-05-08 20:10:58 +0530

    new time added

--- final.py
+++ final.py
@@ -59,14 +59,14 @@ def RunScript():
        
         # time.sleep(4)
 
-        WebDriverWait(bot,2000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="MemberName"]'))).send_keys(email)
+        WebDriverWait(bot,3000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="MemberName"]'))).send_keys(email)
 
         # bot.find_element(By.XPATH, '//*[@id="MemberName"]').send_keys(email)
 
 
         # time.sleep(1)
 
-        WebDriverWait(bot,2000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="iSignupAction"]'))).click()
+        WebDriverWait(bot,3000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="iSignupAction"]'))).click()
 
         # bot.find_element(By.XPATH, '//*[@id="iSignupAction"]').click()
 
@@ -74,7 +74,7 @@ def RunScript():
         # time.sleep(5)
         
         #For Password Imput
-        WebDriverWait(bot,2000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="PasswordInput"]'))).send_keys('Jatin@123')
+        WebDriverWait(bot,3000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="PasswordInput"]'))).send_keys('Jatin@123')
 
         # bot.find_element(By.XPATH, '//*[@id="PasswordInput"]').send_keys('Jatin@123')
 
@@ -82,7 +82,7 @@ def RunScript():
 
         # time.sleep(1)
 
-        WebDriverWait(bot,2000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="iSignupAction"]'))).click()
+        WebDriverWait(bot,3000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="iSignupAction"]'))).click()
 
         # bot.find_element(By.XPATH, '//*[@id="iSignupAction"]').click()
 
@@ -91,45 +91,45 @@ def RunScript():
         # time.sleep(4)
         
         #For Name And Lastname
-        WebDriverWait(bot,2000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="FirstName"]'))).send_keys(firstname)
+        WebDriverWait(bot,3000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="FirstName"]'))).send_keys(firstname)
 
         # bot.find_element(By.XPATH, '//*[@id="FirstName"]').send_keys(firstname)
     
 
         # time.sleep(1)
-        WebDriverWait(bot,2000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="LastName"]'))).send_keys(lastname)
+        WebDriverWait(bot,3000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="LastName"]'))).send_keys(lastname)
 
 
         # bot.find_element(By.XPATH, '//*[@id="LastName"]').send_keys(lastname)
        
 
         # time.sleep(1)
-        WebDriverWait(bot,2000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="iSignupAction"]'))).click()
+        WebDriverWait(bot,3000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="iSignupAction"]'))).click()
 
         # bot.find_element(By.XPATH, '//*[@id="iSignupAction"]').click()
 
         # time.sleep(4)
-        WebDriverWait(bot,2000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="BirthMonth"]/option[6]'))).click()
+        WebDriverWait(bot,3000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="BirthMonth"]/option[6]'))).click()
 
         # bot.find_element(By.XPATH, '//*[@id="BirthMonth"]/option[6]').click()
         # time.sleep(1)
 
-        WebDriverWait(bot,2000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="BirthDay"]/option[10]'))).click()
+        WebDriverWait(bot,3000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="BirthDay"]/option[10]'))).click()
 
         # bot.find_element(By.XPATH, '//*[@id="BirthDay"]/option[10]').click()
         # time.sleep(1)
 
-        WebDriverWait(bot,2000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="BirthYear"]'))).send_keys('1993')
+        WebDriverWait(bot,3000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="BirthYear"]'))).send_keys('1993')
 
         # bot.find_element(By.XPATH, '//*[@id="BirthYear"]').send_keys('1993')
         # time.sleep(1)
 
-        WebDriverWait(bot,2000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="iSignupAction"]'))).click()
+        WebDriverWait(bot,3000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="iSignupAction"]'))).click()
 
         # bot.find_element(By.XPATH, '//*[@id="iSignupAction"]').click()
 
         #OutlookDone? Then ->
-        WebDriverWait(bot,2000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="idBtn_Back"]'))).click()
+        WebDriverWait(bot,3000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="idBtn_Back"]'))).click()
 
         
         time.sleep(5)
@@ -147,42 +147,42 @@ def RunScript():
         bot.get('https://github.com/signup')
 
 
-        WebDriverWait(bot,2000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="email"]'))).send_keys(email)
+        WebDriverWait(bot,3000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="email"]'))).send_keys(email)
 
         time.sleep(2)
 
-        WebDriverWait(bot,2000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="email-container"]/div[2]/button'))).click()
+        WebDriverWait(bot,3000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="email-container"]/div[2]/button'))).click()
 
         time.sleep(2)
 
-        WebDriverWait(bot,2000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="password"]'))).send_keys('Nachiket@1998')
+        WebDriverWait(bot,3000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="password"]'))).send_keys('Nachiket@1998')
 
         time.sleep(2)
 
-        WebDriverWait(bot,2000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="password-container"]/div[2]/button'))).click()
+        WebDriverWait(bot,3000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="password-container"]/div[2]/button'))).click()
 
         githubusername = firstname+lastname+str(random.randrange(10598,100000))
 
         time.sleep(2)
 
-        WebDriverWait(bot,2000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="login"]'))).send_keys(githubusername)
+        WebDriverWait(bot,3000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="login"]'))).send_keys(githubusername)
 
         time.sleep(2)
       
-        WebDriverWait(bot,2000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="username-container"]/div[2]/button'))).click()
+        WebDriverWait(bot,3000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="username-container"]/div[2]/button'))).click()
 
         time.sleep(2)
 
-        WebDriverWait(bot,2000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="opt_in"]'))).send_keys('n')
+        WebDriverWait(bot,3000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="opt_in"]'))).send_keys('n')
 
         time.sleep(2)
 
-        WebDriverWait(bot,2000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="opt-in-container"]/div[2]/button'))).click()
+        WebDriverWait(bot,3000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="opt-in-container"]/div[2]/button'))).click()
 
         time.sleep(3)
 
 
-        # WebDriverWait(bot,2000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="captcha-and-submit-container"]/button'))).click()
+        # WebDriverWait(bot,3000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="captcha-and-submit-container"]/button'))).click()
 
 
 
@@ -193,7 +193,7 @@ def RunScript():
 
         bot.switch_to.window(bot.window_handles[0])
         
-        elee2 = WebDriverWait(bot,2000).until(EC.presence_of_element_located((By.XPATH, "//span[contains(text(),'Continue signing up for GitHub by entering the code below')]")))
+        elee2 = WebDriverWait(bot,3000).until(EC.presence_of_element_located((By.XPATH, "//span[contains(text(),'Continue signing up for GitHub by entering the code below')]")))
                 
         ttext2 = elee2.text
 
@@ -204,16 +204,16 @@ def RunScript():
 
         bot.switch_to.window(bot.window_handles[1])
 
-        WebDriverWait(bot,2000).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[name="launch_code[]"]'))).send_keys(githubcode)
+        WebDriverWait(bot,3000).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[name="launch_code[]"]'))).send_keys(githubcode)
 
 
-        WebDriverWait(bot,2000).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[6]/main/div[3]/div/div/a'))).click()
+        WebDriverWait(bot,3000).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[6]/main/div[3]/div/div/a'))).click()
 
         time.sleep(10)
 
         bot.get('https://github.com/jatinjd17')
 
-        WebDriverWait(bot,2000).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[6]/main/div[2]/div/div[1]/div/div[2]/div[2]/div[1]/div/div/span/form[1]/input[2]'))).click()
+        WebDriverWait(bot,3000).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[6]/main/div[2]/div/div[1]/div/div[2]/div[2]/div[1]/div/div/span/form[1]/input[2]'))).click()
 
         time.sleep(4)
         
@@ -225,7 +225,7 @@ def RunScript():
 
         for i in range(29):
             num = i + 1
-            WebDriverWait(bot,2000).until(EC.presence_of_element_located((By.XPATH,  f'//*[@id="user-repositories-list"]/ul/li[{num}]/div[2]/div[1]/div[2]/form/button'))).click()
+            WebDriverWait(bot,3000).until(EC.presence_of_element_located((By.XPATH,  f'//*[@id="user-repositories-list"]/ul/li[{num}]/div[2]/div[1]/div[2]/form/button'))).click()
             actions = ActionChains(bot) 
             actions.send_keys(Keys.DOWN)
             time.sleep(0.2)
@@ -249,38 +249,38 @@ def RunScript():
             bot.find_element(By.CSS_SELECTOR, 'button._42ft._4jy0._al65._4jy3._4jy1.selected._51sy').click()
         except:
             pass
-        # WebDriverWait(bot,2000).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button._42ft._4jy0._al65._4jy3._4jy1.selected._51sy'))).click()
+        # WebDriverWait(bot,3000).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button._42ft._4jy0._al65._4jy3._4jy1.selected._51sy'))).click()
 
 
-        WebDriverWait(bot,2000).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'a._42ft._4jy0._6lti._4jy6._4jy2.selected._51sy'))).click()
+        WebDriverWait(bot,3000).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'a._42ft._4jy0._6lti._4jy6._4jy2.selected._51sy'))).click()
 
-        WebDriverWait(bot,2000).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[name="reg_email__"]'))).send_keys(email)
+        WebDriverWait(bot,3000).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[name="reg_email__"]'))).send_keys(email)
 
-        WebDriverWait(bot,2000).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[name="reg_email_confirmation__"]'))).send_keys(email)
+        WebDriverWait(bot,3000).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[name="reg_email_confirmation__"]'))).send_keys(email)
 
-        WebDriverWait(bot,2000).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[name="firstname"]'))).send_keys(firstname)
+        WebDriverWait(bot,3000).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[name="firstname"]'))).send_keys(firstname)
 
-        WebDriverWait(bot,2000).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[name="lastname"]'))).send_keys(lastname)
+        WebDriverWait(bot,3000).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[name="lastname"]'))).send_keys(lastname)
 
-        WebDriverWait(bot,2000).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[name="reg_passwd__"]'))).send_keys('Jatin@123')
+        WebDriverWait(bot,3000).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[name="reg_passwd__"]'))).send_keys('Jatin@123')
 
 
         randdate = random.randint(3,20)
 
-        WebDriverWait(bot,2000).until(EC.presence_of_element_located((By.XPATH, f'//*[@id="day"]/option[{randdate}]'))).click()
+        WebDriverWait(bot,3000).until(EC.presence_of_element_located((By.XPATH, f'//*[@id="day"]/option[{randdate}]'))).click()
 
 
         randmonth = random.randint(2,9)
 
-        WebDriverWait(bot,2000).until(EC.presence_of_element_located((By.XPATH, f'//*[@id="month"]/option[{randmonth}]'))).click()
+        WebDriverWait(bot,3000).until(EC.presence_of_element_located((By.XPATH, f'//*[@id="month"]/option[{randmonth}]'))).click()
 
         randyear = random.randint(28, 34)
 
-        WebDriverWait(bot,2000).until(EC.presence_of_element_located((By.XPATH, f'//*[@id="year"]/option[{randyear}]'))).click()
+        WebDriverWait(bot,3000).until(EC.presence_of_element_located((By.XPATH, f'//*[@id="year"]/option[{randyear}]'))).click()
 
-        WebDriverWait(bot,2000).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[value="2"]'))).click()
+        WebDriverWait(bot,3000).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[value="2"]'))).click()
 
-        WebDriverWait(bot,2000).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button[name="websubmit"]'))).click()
+        WebDriverWait(bot,3000).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button[name="websubmit"]'))).click()
 
 
         time.sleep(25)
@@ -289,7 +289,7 @@ def RunScript():
         bot.switch_to.window(bot.window_handles[0])
 
 
-        elee = WebDriverWait(bot,2000).until(EC.presence_of_element_located((By.XPATH, "//span[contains(text(),'is your Facebook confirmation code')]")))
+        elee = WebDriverWait(bot,3000).until(EC.presence_of_element_located((By.XPATH, "//span[contains(text(),'is your Facebook confirmation code')]")))
 
         ttext = elee.text
 
@@ -306,9 +306,9 @@ def RunScript():
         ###############NOTEE##########
         #Put This code using send_keys
 
-        WebDriverWait(bot,2000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="code_in_cliff"]'))).send_keys(facebookcode)
+        WebDriverWait(bot,3000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="code_in_cliff"]'))).send_keys(facebookcode)
 
-        WebDriverWait(bot,2000).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button[name="confirm"]'))).click()
+        WebDriverWait(bot,3000).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button[name="confirm"]'))).click()
                 
         time.sleep(20)
         # Then Click Next
